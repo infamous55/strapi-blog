e806b6bb31eb5572be5400adeec8cdf18058c292
infamous55 <infamouslegend555@gmail.com>
2022-04-12 17:32:35 +0300

    completed comment system

--- src/api/post/controllers/custom-controller.js
+++ src/api/post/controllers/custom-controller.js
@@ -22,4 +22,74 @@ module.exports = createCoreController(modelUid, ({ strapi }) => ({
     });
     ctx.send(comments);
   },
+  async getComment(ctx) {
+    const { id, commentId } = ctx.params;
+    const comment = await strapi.query(modelUid).findOne({
+      where: {
+        id: commentId,
+      },
+      populate: {
+        post: true,
+      },
+    });
+    if (!comment || comment.post.id !== parseInt(id)) {
+      ctx.throw(404, "Comment not found");
+    }
+    delete comment.post;
+    ctx.send(comment);
+  },
+  async updateComment(ctx) {
+    const { id, commentId } = ctx.params;
+    const comment = await strapi.query(modelUid).findOne({
+      where: {
+        id: commentId,
+      },
+      populate: {
+        author: true,
+        post: true,
+      },
+    });
+    if (!comment || comment.post.id !== parseInt(id)) {
+      ctx.throw(404, "Comment not found");
+    }
+    if (comment.author.id !== ctx.state.user.id) {
+      ctx.throw(403, "You are not authorized to perform this action");
+    }
+    const { content } = ctx.request.body;
+    const updatedComment = await strapi.query(modelUid).update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        content,
+      },
+    });
+    ctx.send(updatedComment);
+  },
+  async deleteComment(ctx) {
+    const { id, commentId } = ctx.params;
+    let comment = await strapi.query(modelUid).findOne({
+      where: {
+        id: commentId,
+      },
+      populate: {
+        author: true,
+        post: true,
+      },
+    });
+    if (!comment || comment.post.id !== parseInt(id)) {
+      ctx.throw(404, "Comment not found");
+    }
+    if (comment.author.id !== ctx.state.user.id) {
+      ctx.throw(403, "You are not authorized to perform this action");
+    }
+    await strapi.query(modelUid).delete({
+      where: {
+        id: commentId,
+      },
+    });
+    delete comment.author;
+    delete comment.post;
+    ctx.send(comment);
+  },
 }));
--- src/api/post/routes/custom-routes.js
+++ src/api/post/routes/custom-routes.js
@@ -6,7 +6,7 @@ module.exports = {
       handler: "custom-controller.createComment",
       config: {
         policies: ["global::isAuthenticated"],
-        description: "Create a comment",
+        description: "Create a comment for a post",
       },
     },
     {
@@ -14,7 +14,31 @@ module.exports = {
       path: "/posts/:id/comments",
       handler: "custom-controller.getComments",
       config: {
-        description: "Get all comments for a post",
+        description: "Get all comments of a post",
+      },
+    },
+    {
+      method: "GET",
+      path: "/posts/:id/comments/:commentId",
+      handler: "custom-controller.getComment",
+      config: {
+        description: "Get a comment of a post",
+      },
+    },
+    {
+      method: "PUT",
+      path: "/posts/:id/comments/:commentId",
+      handler: "custom-controller.updateComment",
+      config: {
+        description: "Update one comment of a post",
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/posts/:id/comments/:commentId",
+      handler: "custom-controller.deleteComment",
+      config: {
+        description: "Delete one comment of a post",
       },
     },
   ],
